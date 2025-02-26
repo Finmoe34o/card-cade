ac71fa875cac3d886e8c77f51942f5e9d515f0ac
Finmoe34o <135748853+Finmoe34o@users.noreply.github.com>
2025-02-26 10:49:09 +0000

    jjj

--- components/actionButtons.jsx
+++ components/actionButtons.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 export default function page() {
-    return <div className="flex justify-evenly w-[50vw] h-[20vh]">
+    return <div className="flex absolute left-[25vw] justify-evenly w-[50vw] h-[20vh]">
         <button className="w-[15vw] h-[10vw] bg-gray-950"></button>
         <button className="w-[15vw] h-[10vw] bg-gray-950"></button>
         <button className="w-[15vw] h-[10vw] bg-gray-950"></button>
